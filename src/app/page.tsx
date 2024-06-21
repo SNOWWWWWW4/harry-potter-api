7777d50b3ed6348e7141c50eabaad0e858adb268
SNOWWWWWW4 <lescober7@gmail.com>
2024-06-21 15:20:47 -0700

    added pagination and a transition between sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,150 @@
 'use client';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import styles from './page.module.css';
 import InfoModal from '@/components/modal/InfoModal';
 import { Button } from '@mui/material';
 import { IPotterCharacter } from '@/interface/interface';
 import { callAPI } from '@/utils/data';
 import Image from 'next/image';
+import Lenis from 'lenis';
+import { MotionValue, motion, useScroll, useTransform } from 'framer-motion';
 
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [allCharNames, setAllCharNames] = useState<IPotterCharacter[]>([]);
   const [selectedCharacter, setSelectedCharacter] =
     useState<IPotterCharacter | null>(null);
+  const container = useRef<HTMLElement>(null);
 
   const saveData = async () => {
     const data: IPotterCharacter[] = await callAPI('api/characters');
     setAllCharNames(data);
   };
 
-  useEffect(() => {
-    saveData();
-  }, []);
-
   const handleOpenModal = (character: IPotterCharacter) => {
     setSelectedCharacter(character);
     setIsModalOpen(true);
   };
 
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ['start start', 'end end'],
+  });
+
+  useEffect(() => {
+    saveData();
+
+    const lenis = new Lenis();
+
+    function raf(time: number) {
+      lenis.raf(time);
+
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
+  useEffect(() => {
+    if (isModalOpen) {
+      document.body.classList.add('overflow-hidden');
+    } else {
+      document.body.classList.remove('overflow-hidden');
+    }
+  }, [isModalOpen]);
+
   return (
-    <main className={styles.mainContainer}>
-      <div className='min-h-screen'>
-        <h1 className='text-4xl text-center text-white'>Characters</h1>
-      </div>
-      <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 md:gap-16 lg:grid-cols-4 lg:gap-24 mx-20'>
-        {allCharNames.length > 0 ? (
-          allCharNames.map((char) => (
+    <main className={styles.mainContainer} ref={container}>
+      <Landing scrollYProgress={scrollYProgress} />
+
+      <Table
+        allCharNames={allCharNames}
+        handleOpenModal={handleOpenModal}
+        scrollYProgress={scrollYProgress}
+      />
+
+      {selectedCharacter && (
+        <InfoModal
+          isModalOpen={isModalOpen}
+          setIsModalOpen={setIsModalOpen}
+          character={selectedCharacter}
+        />
+      )}
+    </main>
+  );
+}
+
+const Landing = ({
+  scrollYProgress,
+}: {
+  scrollYProgress: MotionValue<number>;
+}) => {
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.8]);
+  const rotate = useTransform(scrollYProgress, [0, 1], [0, -5]);
+
+  return (
+    <motion.section
+      className='min-h-screen flex flex-col justify-center items-center top-0 sticky bg-[#5d5d66]'
+      style={{ scale, rotate }}
+    >
+      <h1 className='text-4xl text-white font-black'>
+        Learn More About Harry Potter Characters
+      </h1>
+
+      <h2 className='text-white text-xl font-semibold p-4'>
+        Scroll down to see all the characters
+      </h2>
+    </motion.section>
+  );
+};
+
+interface TableProps {
+  allCharNames: IPotterCharacter[];
+  handleOpenModal: (character: IPotterCharacter) => void;
+  scrollYProgress: MotionValue<number>;
+}
+
+const Table = ({
+  allCharNames,
+  handleOpenModal,
+  scrollYProgress,
+}: TableProps) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const charsPerPage = 8;
+
+  const totalChars = allCharNames.length;
+  const totalPages = Math.ceil(totalChars / charsPerPage);
+
+  const startIdx = (currentPage - 1) * charsPerPage;
+  const endIdx = startIdx + charsPerPage;
+  const currentChars = allCharNames.slice(startIdx, endIdx);
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const scale = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const rotate = useTransform(scrollYProgress, [0, 1], [5, 0]);
+
+  return (
+    <motion.section
+      style={{ scale, rotate }}
+      className='bg-[#090923] min-h-screen relative'
+    >
+      <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 px-4 pb-4 pt-12'>
+        {currentChars.length > 0 ? (
+          currentChars.map((char) => (
             <div
               key={char.name}
-              className='bg-slate-800/30 rounded-3xl p-4 flex flex-col justify-center items-center '
+              className='bg-slate-800/30 rounded-3xl p-4 flex flex-col justify-center items-center'
             >
               <h1 className='text-white'>{char.name}</h1>
 
@@ -45,16 +152,16 @@ export default function Home() {
                 <Image
                   src={char.image}
                   alt='image'
-                  width={250}
-                  height={250}
+                  width={200}
+                  height={200}
                   className='aspect-square object-contain my-4'
                 />
               ) : (
                 <Image
                   src={''}
                   alt='has no image'
-                  width={250}
-                  height={250}
+                  width={200}
+                  height={200}
                   className='aspect-square object-contain my-4 bg-slate-600'
                 />
               )}
@@ -68,15 +175,28 @@ export default function Home() {
             <h1>Loading...</h1>
           </div>
         )}
-      </div>
 
-      {selectedCharacter && (
-        <InfoModal
-          isModalOpen={isModalOpen}
-          setIsModalOpen={setIsModalOpen}
-          character={selectedCharacter}
-        />
-      )}
-    </main>
+        {/* Pagination Controls */}
+      </div>
+      <div className='flex justify-center items-center w-full mt-2'>
+        <Button
+          variant='contained'
+          onClick={handlePrevPage}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </Button>
+        <span className='mx-4 text-white'>
+          Page {currentPage} of {totalPages}
+        </span>
+        <Button
+          variant='contained'
+          onClick={handleNextPage}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </Button>
+      </div>
+    </motion.section>
   );
-}
+};
--- src/components/modal/InfoModal.tsx
+++ src/components/modal/InfoModal.tsx
@@ -38,38 +38,38 @@ const InfoModal: React.FC<InfoModalProps> = ({
         <Close />
       </IconButton>
       <DialogContent dividers className='h-[615px]'>
-        <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 '>
+        <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 pt-4'>
           <div className='flex flex-col'>
             <p className='text-pretty my-2'>
-              <strong>Species:</strong> {character.species}
+              <strong>Species:</strong> {character.species ? `${character.species}` : 'N/A'}
             </p>
             <p className='text-pretty my-2'>
-              <strong>Gender:</strong> {character.gender}
+              <strong>Gender:</strong> {character.gender ? `${character.gender}` : 'N/A'}
             </p>
             <p className='text-pretty my-2'>
-              <strong>House:</strong> {character.house}
+              <strong>House:</strong> {character.house ? `${character.house}` : 'N/A'}
             </p>
             <p className='text-pretty my-2'>
-              <strong>Date of Birth:</strong> {character.dateOfBirth}
+              <strong>Date of Birth:</strong> { character.dateOfBirth ? `${character.dateOfBirth}` : 'N/A'}
             </p>
             <p className='text-pretty my-2'>
-              <strong>Ancestry:</strong> {character.ancestry}
+              <strong>Ancestry:</strong> {character.ancestry ? `${character.ancestry}` : 'N/A'}
             </p>
             <p className='text-pretty my-2'>
-              <strong>Eye Colour:</strong> {character.eyeColour}
+              <strong>Eye Colour:</strong> {character.eyeColour ? `${character.eyeColour}` : 'N/A'}
             </p>
             <p className='text-pretty my-2'>
-              <strong>Hair Colour:</strong> {character.hairColour}
+              <strong>Hair Colour:</strong> {character.hairColour? `${character.hairColour}` : 'N/A'}
             </p>
             <p className='text-pretty my-2'>
-              <strong>Wand:</strong> {character.wand.wood} wood,{' '}
-              {character.wand.core} core, {character.wand.length} inches
+              <strong>Wand:</strong> {character.wand.wood !== '' ? `${character.wand.wood} wood,
+              ${character.wand.core} core, ${character.wand.length} inches` : 'N/A'}
             </p>
             <p className='text-pretty my-2'>
               <strong>Patronus:</strong> {character.patronus ? (character.patronus) : 'N/A'}
             </p>
             <p className='text-pretty my-2'>
-              <strong>Actor:</strong> {character.actor}
+              <strong>Actor:</strong> {character.actor ? (character.actor) : 'N/A'}
             </p>
             <p className='text-pretty my-2'>
               <strong>Alive:</strong> {character.alive ? 'Yes' : 'No'}
@@ -80,20 +80,16 @@ const InfoModal: React.FC<InfoModalProps> = ({
               <img
                 src={character.image}
                 alt={character.name}
-                className='w-full h-full object-cover mr-2 my-2 rounded-xl'
+                className='w-full h-[525px] object-cover mr-2 my-2 rounded-xl'
               />
             ) : (
-              <div className='w-full h-full object-cover mr-2 my-2 bg-slate-600 rounded-xl'>
+              <div className='w-full h-full object-cover m-2 bg-slate-600 rounded-xl'>
                 No Image Available
               </div>
             )}
           </div>
         </div>
       </DialogContent>
-
-      <DialogActions>
-        <Button onClick={() => setIsModalOpen(false)}>Close</Button>
-      </DialogActions>
     </Dialog>
   );
 };
